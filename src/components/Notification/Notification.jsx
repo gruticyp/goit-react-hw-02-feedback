8bbff148016323c4e7363de3cf3305636051b2ff
gruticyp <gruticyp@gmail.com>
2022-07-17 06:12:12 +0300

    1

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,18 @@
+import PropTypes from  'prop-types';
+import styles from './notification.module.css';
+
+function Notification ({message}) {
+    return (
+        <div className={styles.wrapper}>
+            <p className={styles.text}>{message}</p>
+        </div>
+    )
+}
+
+Notification.propTypes = { 
+    message: PropTypes.string.isRequired,
+}
+
+
+
+export default Notification;
